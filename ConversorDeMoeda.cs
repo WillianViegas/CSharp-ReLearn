cb405365d48d5fc6107329de2c3cc28071f46944
Will <willianwed98@gmail.com>
2020-09-04 17:36:33 -0300

    Membros estaticos exemplo e exercicio

---
+++ ConversorDeMoeda.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Course
+{
+    class ConversorDeMoeda
+    {
+        public static double RealParaDolar(double real, double dolar)
+        {
+            return real * dolar;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,58 +7,25 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            //Retangulo calculos
-            /* Retangulo ret = new Retangulo();
-             Console.WriteLine("Entre a largura e altura do retangulo:");
-             ret.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-             ret.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            /*Console.Write("Entre o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-             Console.WriteLine("AREA: " + ret.Area().ToString("F2",CultureInfo.InvariantCulture));
-             Console.WriteLine("PERIMETRO: " + ret.Perimetro().ToString("F2", CultureInfo.InvariantCulture));
-             Console.WriteLine("DIAGONAL: " + ret.Diagonal().ToString("F2", CultureInfo.InvariantCulture));*/
+            double circ = Calculadora.Circunferencia(raio);
+            double volume = Calculadora.Volume(raio);
+
+            Console.WriteLine("Circunferencia: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Pi: " + Calculadora.Pi.ToString("F2", CultureInfo.InvariantCulture));*/
+
+            Console.Write("Cotacao dolar: ");
+            double dolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantos dolares vai comprar: ");
+            double real = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Valor a ser pago em real : " 
+                + ConversorDeMoeda.RealParaDolar(real, dolar).ToString("F2", CultureInfo.InvariantCulture));
 
 
-            //Calculo salario funcionario
-            /* Funcionario f1 = new Funcionario();
-             Console.Write("Nome: ");
-             f1.Nome = Console.ReadLine();
-             Console.Write("Salário bruto: ");
-             f1.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-             Console.Write("Imposto: ");
-             f1.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-             Console.WriteLine("Funcionário: " + f1);
-
-             Console.WriteLine("Digite a porcentagem para aumentar o salário: ");
-             double perc = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-             f1.AumentarSalario(perc);
-
-             Console.WriteLine("Funcionário: " + f1);*/
-
-            //nota Aluno
-            Aluno aluno = new Aluno();
-
-            Console.Write("Nome do Aluno:");
-            aluno.Nome = Console.ReadLine();
-            Console.Write("Nota 1: ");
-            aluno.N1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            Console.Write("Nota 2: ");
-            aluno.N2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            Console.Write("Nota 3: ");
-            aluno.N3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-            Console.WriteLine("NOTA FINAL: " + aluno.NotaFinal().ToString("F2", CultureInfo.InvariantCulture));
-            if(aluno.NotaFinal() >= 60.0)
-            {
-                Console.WriteLine("Aprovado");
-            }
-            else
-            {
-                Console.WriteLine("Reprovado");
-                Console.WriteLine("Faltaram " 
-                    + aluno.PontosFaltantes(aluno.NotaFinal()).ToString("F2", CultureInfo.InvariantCulture)
-                     + " Pontos");
-            }
         }
     }
 }
