dc96d5a396db8e08caa277de42b626c2e6485f89
Will <willianwed98@gmail.com>
2020-09-14 21:35:04 -0300

    Exercicio Matriz

--- Program.cs
+++ Program.cs
@@ -8,41 +8,59 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            int n = int.Parse(Console.ReadLine());
+            string[] values = Console.ReadLine().Split(' ');
+            int m = int.Parse(values[0]);
+            int n = int.Parse(values[1]);
 
-            int[,] mat = new int[n, n];
+            int[,] mat = new int[m, n];
 
-            for(int i =0; i < n; i++)
+            for(int i =0; i < m; i++)
             {
-                string[] values = Console.ReadLine().Split(' ');
+                values = Console.ReadLine().Split(' ');
                 for(int j = 0; j < n; j++)
                 {
                     mat[i, j] = int.Parse(values[j]);
                 }
             }
 
-            Console.WriteLine("Main diagonal:");
-            for(int i = 0; i < n; i++)
-            {
-                Console.Write(mat[i,i] + " ");
-            }
-            Console.WriteLine();
+            int x = int.Parse(Console.ReadLine());
 
-            int count = 0;
 
-            for(int i = 0; i < n; i++)
+            for(int i = 0; i < m; i++)
             {
                 for(int j = 0; j < n; j++)
                 {
-                    if(mat[i, j] < 0)
+                    if(mat[i, j] == x)
                     {
-                        count++;
+                        Console.WriteLine("Position " + i + "," + j + ":");
+                        
+                        if(j > 0)
+                        {
+                        Console.WriteLine("Left: " + mat[i, j - 1]);
+                        }
+                        if(j < n -1)
+                        {
+                        Console.WriteLine("Right: " + mat[i, j + 1]);
+                        }
+                       
+                        if(i < m -1)
+                        {
+                        Console.WriteLine("Down: " + mat[i + 1, j]);
+                        }
+                        if(i > 0)
+                        {
+                        Console.WriteLine("Up: " + mat[i - 1, j]);
+                        }
                     }
                 }
+                    Console.WriteLine();
             }
-
-            Console.WriteLine("Negative Numbers: ");
-            Console.WriteLine(count);
         }
     }
 }
+/*3 4
+10 8 15 12
+21 11 23 8
+14 5 13 19
+8*/
+
